ff535e20edde1f4fa8d25fb1a7b8bc43ce046455
shark27 <44381346+jslzr27@users.noreply.github.com>
2019-02-09 00:42:32 -0500

    style added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.fixed-action-btn');
+    var elems = document.querySelectorAll('.fixed-action-btn',);
     var instances = M.FloatingActionButton.init(elems, {
         direction: 'top',
         hoverEnabled: true,
@@ -7,13 +7,19 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
 
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+  });
+
+
   var linkedIn = document.querySelectorAll('#linkedin');
   var github = document.querySelectorAll('#github');
 
   $(linkedIn).on("click", function() {
       window.open("https://www.linkedin.com/in/jesus-salazar-484837173/");
-  })
+  });
 
   $(github).on("click", function() {
     window.open("https://github.com/jslzr27");
-})
+  });
