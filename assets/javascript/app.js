84eb9049d03a4e75cd88d4f58e88366517a399af
shark27 <44381346+jslzr27@users.noreply.github.com>
2019-02-08 19:07:17 -0500

    files added, updated look of the site

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, {
+        direction: 'top',
+        hoverEnabled: true,
+        toolbarEnabled: false
+    });
+  });
+
